94062f08ee3afd0d065b0661b7141d8ab8e89a4a
fsship <1844695939@qq.com>
2015-04-17 20:02:20 +0800

    add model

--- recruit/Common/Model/AssociationListModel.class.php
+++ recruit/Common/Model/AssociationListModel.class.php
@@ -17,4 +17,18 @@ class AssociationListModel extends Model {
         $result = $this->where($condition)->find();
         return $result['associationName'];
     }
+
+    public function getQuestions($id) {
+        $condition['id'] = $id;
+        $result = $this->where($condition)->find();
+        $questions = array();
+        $k = 0;
+        for($i = 1; $i <= 3; $i++) {
+            if (isset($result['quest'.$i])) {
+                $questions[$k] = $result['quest'.$i];
+                $k++;
+            }
+        }
+        return $questions;
+    }
 }
